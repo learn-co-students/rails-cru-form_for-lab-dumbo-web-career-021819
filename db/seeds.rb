3a3641676a4670103a27a83575a1ed6768514f17
Rhohoman <rhohoman@gmail.com>
2019-03-19 19:01:00 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+artist1 = Artist.create([name:"John",bio:"knee"])
+artist2 = Artist.create([name:"Dum",bio:"Mee"])
+
+
+genre1 = Genre.create([name:"Swag"])
+genre2 = Genre.create([name:"Duck"])
+
+song1 = Song.create([name:"Can",artist_id: artist1[0].id,genre_id: genre1[0].id])
+song3 = Song.create([name:"Not Can",artist_id: artist2[0].id,genre_id: genre1[0].id])
+song2 = Song.create([name:"Really Not a Can",artist_id: artist2[0].id,genre_id: genre2[0].id])
